a80536da5ed6ff65d9dd15c5203e9705bad53d91
shashankvemuri <shashank.vemuri1@gmail.com>
2024-04-11 03:52:47 -0400

    background.js listener

---
+++ public/background.js
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed!');
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message);
+});
